c02c946b214a7616384f6444368c2320f24327aa
Pierre Keane <pke.webdev@gmail.com>
2026-04-01 14:27:24 +0200

    navbar now responds to switching

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,4 +2,18 @@ fetch('/views/shared/sharedNavbar.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('navbar-container').innerHTML = data;
+
+    const path = window.location.pathname;
+    const links = document.querySelectorAll("#Navbar a h3")
+
+    links.forEach( (link) => {
+      link.classList.remove("font-extrabold");
+      link.classList.add("font-medium");
+
+      const href = link.parentElement.getAttribute("href");
+      if (path.endsWith(href)) {
+        link.classList.remove("font-medium");
+        link.classList.add("font-extrabold");
+      }
+    })
 });
